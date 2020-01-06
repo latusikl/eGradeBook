883792fa6742f22a90eca376bfb8f6ea6dd3dc52
JChrobak <jonatanchrobak@gmail.com>
2020-01-06 20:48:22 +0100

    Added redirect after login

--- src/main/java/pl/polsl/egradebook/controller/MainController.java
+++ src/main/java/pl/polsl/egradebook/controller/MainController.java
@@ -1,10 +1,15 @@
 package pl.polsl.egradebook.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Collection;
+
 
 @Controller
 public class MainController {
@@ -14,7 +19,25 @@ public class MainController {
 	public String index(){
 		return "login.html";
 	}
-	
+	//redirect here after successful login, url temporary, better name pending
+	@GetMapping("/authredir")
+	public ModelAndView authredir()
+	{
+		//get collection of all authorities, which contains allowed urls
+		Collection<? extends GrantedAuthority> authorities=SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		//admin
+		if(authorities.contains(new SimpleGrantedAuthority("/user/show/all")))
+			return new ModelAndView("redirect:/user/show/all");
+		//teacher
+		else if(authorities.contains(new SimpleGrantedAuthority("/teacher")))
+			return new ModelAndView("redirect:/teacher");
+		//student
+		else if(authorities.contains(new SimpleGrantedAuthority("/student")))
+			return new ModelAndView("redirect:/student");
+		//else
+		else
+			return new ModelAndView("redirect:/");
+	}
 	@GetMapping("/login")
 	public String login(){
 		return "login.html";

a2166f12adf227527e323434593d69ffd0efbeb3
syphzur <krzysztof.bol@wp.pl>
2020-01-07 21:52:30 +0100

    fixed entities

--- src/main/java/pl/polsl/egradebook/controller/MainController.java
+++ src/main/java/pl/polsl/egradebook/controller/MainController.java
@@ -11,40 +11,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
 @Controller
 public class MainController {
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	@GetMapping("/")
-	public String index(){
-		return "login.html";
-	}
-	//redirect here after successful login, url temporary, better name pending
-	@GetMapping("/authredir")
-	public String authredir(Authentication authentication)
-	{
-		//get collection of all authorities, which contains allowed urls
-		//admin
-		if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("/user/show/all")))
-			return "redirect:/user/show/all";
-		//teacher
-		else if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("/teacher")))
-			return "redirect:/teacher";
-		//student
-		else if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("/student")))
-			return "redirect:/student";
-		//else
-		else
-			return "redirect:/";
-	}
-	@GetMapping("/login")
-	public String login(){
-		return "login.html";
-	}
-	
-	@GetMapping("/error/403")
-	public String getAccessDeniedPage(){
-		return "access-denied.html";
-	}
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @GetMapping("/")
+    public String index() {
+        return "login.html";
+    }
+
+    //redirect here after successful login, url temporary, better name pending
+    @GetMapping("/authredir")
+    public String authredir(Authentication authentication) {
+        //get collection of all authorities, which contains allowed urls
+        //admin
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("/user/show/all")))
+            return "redirect:/user/show/all";
+            //teacher
+        else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("/teacher")))
+            return "redirect:/teacher";
+            //student
+        else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("/student")))
+            return "redirect:/student";
+            //else
+        else
+            return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @GetMapping("/error/403")
+    public String getAccessDeniedPage() {
+        return "access-denied.html";
+    }
 }
--- src/main/java/pl/polsl/egradebook/controller/TeacherController.java
+++ src/main/java/pl/polsl/egradebook/controller/TeacherController.java
@@ -1,6 +1,5 @@
 package pl.polsl.egradebook.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;

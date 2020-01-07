57fb4f4a22b951596cefa12a2efb4960008b0d16
latusikl <latusikl@gmail.com>
2020-01-07 23:11:04 +0100

    Changed user to admin. Started password change function.

--- src/main/java/pl/polsl/egradebook/controller/TeacherController.java
+++ src/main/java/pl/polsl/egradebook/controller/TeacherController.java
@@ -13,37 +13,37 @@ import pl.polsl.egradebook.model.repositories.PresenceRepository;
 import pl.polsl.egradebook.model.repositories.UserRepository;
 
 
-@Controller
-@RequestMapping("/teacher")
-public class TeacherController {
-
-    private final StudentRepository studentRepository;
-
-    private final UserRepository userRepository;
-
-    private final GradeRepository gradeRepository;
-
-    private final PresenceRepository presenceRepository;
-    
-    public TeacherController(StudentRepository studentRepository, UserRepository userRepository, GradeRepository gradeRepository, PresenceRepository presenceRepository) {
-        this.studentRepository = studentRepository;
-        this.userRepository = userRepository;
-        this.gradeRepository = gradeRepository;
-        this.presenceRepository = presenceRepository;
-    }
-    
-    @GetMapping()
-    @PreAuthorize("hasAuthority('/teacher')")
-    public String viewGradesAndAttendance(Authentication authentication, Model model) {
-        String userName = authentication.getName();
-        User loggedTeacher = userRepository.findUserByUserName(userName);
-        Iterable<Student> classStudents = studentRepository.findAll();
-        model.addAttribute("teacher", loggedTeacher);
-        model.addAttribute("grades", gradeRepository.findAllByOrderByStudent_User_SurnameAsc());
-        model.addAttribute("attendance", presenceRepository.findAll());
-        model.addAttribute("students", studentRepository.findAll());
-
-        return "teacher-view";
-    }
-
-}
+//@Controller
+//@RequestMapping("/teacher")
+//public class TeacherController {
+//
+//    private final StudentRepository studentRepository;
+//
+//    private final UserRepository userRepository;
+//
+//    private final GradeRepository gradeRepository;
+//
+//    private final PresenceRepository presenceRepository;
+//
+//    public TeacherController(StudentRepository studentRepository, UserRepository userRepository, GradeRepository gradeRepository, PresenceRepository presenceRepository) {
+//        this.studentRepository = studentRepository;
+//        this.userRepository = userRepository;
+//        this.gradeRepository = gradeRepository;
+//        this.presenceRepository = presenceRepository;
+//    }
+//
+//    @GetMapping()
+//    @PreAuthorize("hasAuthority('/teacher')")
+//    public String viewGradesAndAttendance(Authentication authentication, Model model) {
+//        String userName = authentication.getName();
+//        User loggedTeacher = userRepository.findUserByUserName(userName);
+//        Iterable<Student> classStudents = studentRepository.findAll();
+//        model.addAttribute("teacher", loggedTeacher);
+//        model.addAttribute("grades", gradeRepository.findAllByOrderByStudent_User_SurnameAsc());
+//        model.addAttribute("attendance", presenceRepository.findAll());
+//        model.addAttribute("students", studentRepository.findAll());
+//
+//        return "teacher-view";
+//    }
+//
+//}
